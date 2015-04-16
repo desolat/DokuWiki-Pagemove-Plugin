desolat/DokuWiki-Pagemove-Plugin

commit c50d2a48e18f43f87725c9a868eba421bb1e6baf

When the page is locked, rewrite it for show only

helper/rewrite.php
@@ -239,7 +239,7 @@ class helper_plugin_move_rewrite extends DokuWiki_Plugin {
      * @param string|null $text Old content of the page. When null is given the content is loaded from disk
      * @return string|bool The rewritten content, false on error
      */
-    public function rewritePage($id, $text = null) {
+    public function rewritePage($id, $text = null, $save = true) {
         $meta = $this->getMoveMeta($id);
         if(is_null($text)) {
             $text = rawWiki($id);
@@ -251,19 +251,21 @@ class helper_plugin_move_rewrite extends DokuWiki_Plugin {
 
             $changed = ($old_text != $text);
             $file    = wikiFN($id, '', false);
-            if(is_writable($file) || !$changed) {
-                if($changed) {
-                    // Wait a second when the page has just been rewritten
-                    $oldRev = filemtime(wikiFN($id));
-                    if($oldRev == time()) sleep(1);
+            if ($save === true) {
+                if(is_writable($file) || !$changed) {
+                    if($changed) {
+                        // Wait a second when the page has just been rewritten
+                        $oldRev = filemtime(wikiFN($id));
+                        if($oldRev == time()) sleep(1);
 
-                    saveWikiText($id, $text, $this->symbol . ' ' . $this->getLang('linkchange'), $this->getConf('minor'));
+                        saveWikiText($id, $text, $this->symbol . ' ' . $this->getLang('linkchange'), $this->getConf('minor'));
+                    }
+                    $this->unsetMoveMeta($id);
+                } else {
+                    // FIXME: print error here or fail silently?
+                    msg('Error: Page ' . hsc($id) . ' needs to be rewritten because of page renames but is not writable.', -1);
+                    return false;
                 }
-                $this->unsetMoveMeta($id);
-            } else {
-                // FIXME: print error here or fail silently?
-                msg('Error: Page ' . hsc($id) . ' needs to be rewritten because of page renames but is not writable.', -1);
-                return false;
             }
         }
 

action/rewrite.php
@@ -36,7 +36,10 @@ class action_plugin_move_rewrite extends DokuWiki_Action_Plugin {
         if($event->data[3]) return;
 
         // only rewrite if not in move already
-        if(helper_plugin_move_rewrite::isLocked()) return;
+        $save = true;
+        if(helper_plugin_move_rewrite::isLocked()) {
+            $save = false;
+        }
 
         $id = $event->data[2];
         if($event->data[1]) $id = $event->data[1] . ':' . $id;
@@ -67,7 +70,7 @@ class action_plugin_move_rewrite extends DokuWiki_Action_Plugin {
         $helper = plugin_load('helper', 'move_rewrite', true);
         if(!is_null($helper)) {
             $stack[$id]    = true;
-            $event->result = $helper->rewritePage($id, $event->result);
+            $event->result = $helper->rewritePage($id, $event->result, $save);
             unset($stack[$id]);
         }
     }

commit 97cecfe2b15f0733dc937fba1bf36eaeef4e0d65

Fix failing rewrites because the page was still locked
The change introduced in c50d2a48e18f43f87725c9a868eba421bb1e6baf caused
the rewrite function to delete the move meta data, even so the change
hasn't yet been written to disk.

helper/plan.php
@@ -276,12 +276,7 @@ class helper_plugin_move_plan extends DokuWiki_Plugin {
         }
         $this->plan = array();
         $this->loadOptions();
-        global $conf;
-        $lockfile = $conf['lockdir'] . 'move.lock';
-        if (file_exists($lockfile)) {
-            unlink($lockfile);
-        }
-        unset($GLOBALS['PLUGIN_MOVE_WORKING']);
+        helper_plugin_move_rewrite::removeAllLocks();
     }
 
     /**
@@ -400,7 +395,7 @@ class helper_plugin_move_plan extends DokuWiki_Plugin {
             return max($todo, 1); // force one more call
         }
 
-        helper_plugin_move_rewrite::removeLock();
+        helper_plugin_move_rewrite::removeAllLocks();
 
         if($this->options['autorewrite'] && @filesize($this->files['affected']) > 1) {
             $todo = $this->stepThroughAffectedPages();

helper/rewrite.php
@@ -179,6 +179,20 @@ class helper_plugin_move_rewrite extends DokuWiki_Plugin {
         }
     }
 
+    /**
+     * Allow rewrites in this process again.
+     *
+     * @author Michael Große <grosse@cosmocode.de>
+     */
+    public static function removeAllLocks() {
+        global $conf;
+        $lockfile = $conf['lockdir'] . 'move.lock';
+        if (file_exists($lockfile)) {
+            unlink($lockfile);
+        }
+        unset($GLOBALS['PLUGIN_MOVE_WORKING']);
+    }
+
 
     /**
      * Rewrite a text in order to fix the content after the given moves.