1430f9f82af81bcfb053d455925b233fb0122e63
Michael Große <grosse@cosmocode.de>
2015-04-20 15:17:14 +0200

    Add unittests for findMissingDocuments

---
+++ _test/findMissingDocuments.test.php
@@ -0,0 +1,100 @@
+<?php
+
+
+class helper_plugin_move_plan_findMissingDocuments_mock extends helper_plugin_move_plan {
+
+    public function findMissingDocuments($src, $dst,  $type = self::TYPE_PAGES) {
+        parent::findMissingDocuments($src, $dst, $type);
+    }
+
+    public function getTmpstore() {
+        return $this->tmpstore;
+    }
+
+}
+
+
+/**
+ * Test cases for helper_plugin_move_plan::stepThroughDocuments function of the move plugin
+ *
+ * @group plugin_move
+ * @group plugin_move_unittests
+ * @group plugins
+ * @group unittests
+ * @covers helper_plugin_move_plan::findMissingDocuments
+ */
+class plugin_move_findMissingPages_test extends DokuWikiTest {
+
+    protected $pluginsEnabled = array('move',);
+    /** @var  helper_plugin_move_plan_findMissingDocuments_mock $plan */
+    protected $plan;
+
+    /**
+     * @coversNothing
+     */
+    public function setUp() {
+        parent::setUp();
+        $this->plan = new helper_plugin_move_plan_findMissingDocuments_mock();
+    }
+
+
+    /**
+     * @coversNothing
+     */
+    public function tearDown() {
+        global $conf;
+        $indexfn = $conf['indexdir'].'/relation_references_w.idx';
+        if(file_exists($indexfn)) {
+            unlink($indexfn);
+        }
+        $this->plan->abort();
+        parent::tearDown();
+    }
+
+
+    function test_findMissingPages_empty () {
+        $this->plan->findMissingDocuments('oldns','newns:');
+        $tmpstore = $this->plan->getTmpstore();
+        $this->assertSame(array(),$tmpstore['miss']);
+    }
+
+    function test_findMissingPages_missingPage_default () {
+        saveWikiText('start','[[oldns:missing]]','test edit');
+        idx_addPage('start');
+        $this->plan->findMissingDocuments('oldns','newns:');
+        $tmpstore = $this->plan->getTmpstore();
+        $this->assertSame(array('oldns:missing' => 'newns:missing',),$tmpstore['miss']);
+    }
+
+    function test_findMissingPages_missingPage_explicit () {
+        saveWikiText('start','[[oldns:missing]]','test edit');
+        idx_addPage('start');
+        $this->plan->findMissingDocuments('oldns','newns:',helper_plugin_move_plan::TYPE_PAGES);
+        $tmpstore = $this->plan->getTmpstore();
+        $this->assertSame(array('oldns:missing' => 'newns:missing',),$tmpstore['miss']);
+    }
+
+    function test_findMissingPages_missingPage_integrated () {
+        saveWikiText('oldns:start','[[oldns:missing]] {{oldns:missing.png}}','test edit');
+        idx_addPage('oldns:start');
+        $this->plan->addPageNamespaceMove('oldns', 'newns');
+        $this->plan->addMediaNamespaceMove('oldns', 'newns');
+
+        $this->plan->commit();
+
+        $missing_file = file(TMP_DIR . '/data/meta/__move_missing');
+        $this->assertSame(array("oldns:missing\tnewns:missing\n",),$missing_file,'new configuration fails');
+
+        $missing_media_file = file(TMP_DIR . '/data/meta/__move_missing_media');
+        $this->assertSame(array("oldns:missing.png\tnewns:missing.png\n",),$missing_media_file,'new configuration fails');
+
+    }
+
+    function test_findMissingPages_missingMedia () {
+        saveWikiText('start','{{oldns:missing.png}}','test edit');
+        idx_addPage('start');
+        $this->plan->findMissingDocuments('oldns','newns:',helper_plugin_move_plan::TYPE_MEDIA);
+        $tmpstore = $this->plan->getTmpstore();
+        $this->assertSame(array('oldns:missing.png' => 'newns:missing.png',),$tmpstore['miss_media']);
+    }
+}
