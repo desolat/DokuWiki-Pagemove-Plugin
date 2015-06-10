ad294df658cc56fcaac6323740e0db12377a7a7a
Andre Dierick <ajmdierick@gmail.com>
2015-06-10 16:20:37 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -1,30 +1,22 @@
 <?php
+
 /**
- * nederlands (Dutch) language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Gary Owen <gary@isection.co.uk>
+ * 
+ * @author Gary Owen <gary@isection.co.uk>
+ * @author Andre Dierick <ajmdierick@gmail.com>
  */
-
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'Pagina Verplaatsen/Hernoemen...';
-$lang['desc'] = 'Pagina Verplaatsen/Hernoemen Plugin';
-
-//$lang['notexist']   = 'Dit onderwerp bestaat nog niet';
-$lang['notwrite']   = 'U heeft niet voldoende rechten om deze pagina te wijzigen';
-$lang['badns']      = 'De namespace bevat een niet-toegestaan teken.';
-$lang['badname']    = 'De paginanaam bevat een niet-toegestaan teken.';
-$lang['nochange']   = 'De paginanaam en -namespace zijn ongewijzigd.';
-$lang['existing1']  = 'Het document met naam %s bestaat al in %s';
-$lang['root']       = '[Hoofdnamespace]';
-$lang['current']    = '(Huidig)';
-//$lang['norights']   = 'U heeft niet voldoende rechten om een of meerdere referenties aan te passen.';
-$lang['filelocked']  = 'Deze file is geblokkeerd - probeer later opnieuw.';
-$lang['linkchange1'] = 'Gelinkt aan %s gewijzigd naar %s';
-$lang['newname']     = 'Nieuwe naam document:';
-$lang['targetns']    = 'Selecteer namespace van het doel:';
+$lang['menu']                  = 'Pagina Verplaatsen/Hernoemen...';
+$lang['inprogress']            = '(verplaatsen verwacht)';
+$lang['notexist']              = 'De pagina %s bestaat niet';
+$lang['norights']              = 'U hebt onvoldoende permissies om %s aan te passen';
+$lang['filelocked']            = 'Deze file is geblokkeerd - probeer later opnieuw.';
+$lang['exists']                = 'Pagina %s kan niet worden verplaatst naar %s, het doel bestaat al';
+$lang['notargetperms']         = 'U heeft niet voldoende permissies om pagina %s aan te maken';
+$lang['medianotexist']         = 'Het media bestand %s bestaat niet';
+$lang['nomediarights']         = 'U heeft niet voldoende permissies om pagina %s te verwijderen';
+$lang['nomediatargetperms']    = 'U heeft niet voldoende permissies om het media bestand %s aan te maken ';
+$lang['renamed']               = 'Pagina naam van %s gewijzigd in %s';
+$lang['moved']                 = 'Pagina verplaatst van %s naar %s';
+$lang['move_rename']           = 'pagina verplaatst en hernoemd van %s naar %s';
+$lang['root']                  = '[Hoofdnamespace]';
