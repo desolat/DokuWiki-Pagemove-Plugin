f2f492f352b726dd9cb4912d3e742957d1e7c11e
Daniel Raknes <rada@jbv.no>
2015-06-29 15:15:04 +0200

    translation update

--- lang/no/lang.php
+++ lang/no/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Daniel <daniel.raknes@hotmail.no>
+ * @author Daniel Raknes <rada@jbv.no>
  */
 $lang['menu']                  = 'Flytt sider og navnerom';
 $lang['inprogress']            = '(flytting avventer)';
@@ -11,3 +12,19 @@ $lang['treelink']              = 'I tillegg til dette enkle skjemaet kan du ogs�
 $lang['notexist']              = 'Siden %s eksisterer ikke';
 $lang['norights']              = 'Du har ikke nok rettigheter til å endre %s.';
 $lang['filelocked']            = 'Side %s er låst. Prøv igjen senere.';
+$lang['notchanged']            = 'Det er ikke lagt inn nytt lagringssted for %s (lagringsstedet er uendret).';
+$lang['exists']                = 'Siden %s kan ikke flyttes til %s, mål finnes allerede.';
+$lang['notargetperms']         = 'Du har ikke rettigheter til å lage siden %s.';
+$lang['medianotexist']         = 'Media-filen %s eksisterer ikke';
+$lang['nomediarights']         = 'Du har ikke rettigheter til å slette %s.';
+$lang['metamoveerror']         = 'Metafilene for siden %s kunne ikke flyttes.';
+$lang['mediamoveerror']        = 'Flytting av mediafilen %s feilet';
+$lang['moved']                 = 'Siden ble flyttet fra %s til %s';
+$lang['delete']                = 'Slettet av move plugin';
+$lang['btn_start']             = 'Start';
+$lang['btn_continue']          = 'Fortsett';
+$lang['btn_retry']             = 'Prøv på nytt';
+$lang['btn_skip']              = 'Hopp over';
+$lang['btn_abort']             = 'Avbryt';
+$lang['movepage']              = 'Flytt side';
+$lang['movens']                = 'Flytt navnerom';
