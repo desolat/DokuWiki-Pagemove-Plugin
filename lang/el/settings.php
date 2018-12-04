475755158abfb07312c8cd385702286d5d84ac6e
Katerina Katapodi <extragold1234@hotmail.com>
2018-12-04 18:25:39 +0100

    translation update

---
+++ lang/el/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <extragold1234@hotmail.com>
+ */
+$lang['autoskip']              = 'Ενεργοποιήστε το αυτόματο σύστημα παράλειψης λαθών στις μετακινήσεις ονόματος αρχείου κατ\'επιλογή';
+$lang['autorewrite']           = 'Ενεργοποιήστε τον αυτόματο σύνδεσμο γραφής μετά την μετακίνηση θέσης ονόματος αρχείου κατ\'επιλογήν';
